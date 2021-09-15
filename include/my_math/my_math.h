a49d77096999c6198019df6ed1dc05af52fa4fa7
Denley Hsiao <denley@justtodo.com>
2021-09-15 17:10:52 +0800

    Add my_math files.

---
+++ include/my_math/my_math.h
@@ -0,0 +1,6 @@
+#ifndef __MY_MATH_MY_MATH_H__
+#define __MY_MATH_MY_MATH_H__
+
+double mysqrt(double number);
+
+#endif
---
+++ src/my_math.cpp
@@ -0,0 +1,7 @@
+#include "my_math/my_math.h"
+#include <cmath>
+
+double mysqrt(double number)
+{
+  return std::sqrt(number) - 1;
+}
